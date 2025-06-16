6beb81f0562137c0d12cc4cd63096f526172cde7
Priya Bagus <priyabagus.a29@gmail.com>
2025-06-16 10:31:55 +0700

    chore: adjust package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+import config from '@mystaline/mysta-presets/raijin/colors.config.json';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: config,
+    },
   },
   plugins: [],
 };
